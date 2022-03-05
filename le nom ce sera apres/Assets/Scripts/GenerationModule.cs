d1bf2ff1a815745082d0228d7500472232f85615
MathieuMZR <m.meziere@rubika-edu.com>
2022-03-05 03:57:21 +0100

    Generation Procedurale Terminee

--- le nom ce sera apres/Assets/Scripts/GenerationModule.cs
+++ le nom ce sera apres/Assets/Scripts/GenerationModule.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -9,34 +10,26 @@ public class GenerationModule : MonoBehaviour
 {
     public List<GameObject> prefabList;
 
-    private GameObject nextSpawnPrefab;
+    private GameObject _nextSpawnPrefab;
     public GameObject nextSpawnOffset;
-    private List<GameObject> oldSpawnedPrefab;
-    
-    private int randomFloat;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private int _randomFloat;
+
+    public StackGrids stackGrids;
+
+    private int _indexList;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("YO");
-            randomFloat = Random.Range(0, prefabList.Count);
-            nextSpawnPrefab = prefabList[randomFloat];
+            _randomFloat = Random.Range(0, prefabList.Count);
+            _nextSpawnPrefab = prefabList[_randomFloat];
 
-            GameObject spawnedPrefab = Instantiate(nextSpawnPrefab);
+            GameObject spawnedPrefab = Instantiate(_nextSpawnPrefab);
             spawnedPrefab.transform.position = nextSpawnOffset.transform.position;
+            
+            stackGrids.oldSpawnedPrefab.Add(spawnedPrefab);
         }
     }
 
@@ -44,7 +37,21 @@ public class GenerationModule : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            
+            if (stackGrids.oldSpawnedPrefab.Any())
+            {
+                if (_indexList > 2)
+                {
+                    Destroy(stackGrids.basePrefab);
+                    Destroy(stackGrids.oldSpawnedPrefab[0]);
+                    stackGrids.oldSpawnedPrefab.RemoveAt(0);
+                }
+            }
         }
+    }
+
+    private void Update()
+    {
+        _indexList = stackGrids.oldSpawnedPrefab.Count;
+        Debug.Log(stackGrids.oldSpawnedPrefab.Count);
     }
 }
