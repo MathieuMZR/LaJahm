312eedd80b68173460403576705427cbd5610185
MathieuMZR <m.meziere@rubika-edu.com>
2022-03-05 01:17:28 +0100

    Generation Procedurale

--- le nom ce sera apres/Assets/Scripts/GenerationModule.cs
+++ le nom ce sera apres/Assets/Scripts/GenerationModule.cs
@@ -1,9 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class GenerationModule : MonoBehaviour
 {
+    public List<GameObject> prefabList;
+
+    private GameObject nextSpawnPrefab;
+    public GameObject nextSpawnOffset;
+    private List<GameObject> oldSpawnedPrefab;
+    
+    private int randomFloat;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +25,26 @@ public class GenerationModule : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("YO");
+            randomFloat = Random.Range(0, prefabList.Count);
+            nextSpawnPrefab = prefabList[randomFloat];
+
+            GameObject spawnedPrefab = Instantiate(nextSpawnPrefab);
+            spawnedPrefab.transform.position = nextSpawnOffset.transform.position;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+        }
     }
 }
