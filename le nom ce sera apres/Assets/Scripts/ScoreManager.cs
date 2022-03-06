3b0a64b749e415dac111d8651a6e4b63afa25d01
MathieuMZR <m.meziere@rubika-edu.com>
2022-03-06 08:02:04 +0100

    Prefab Player Collider

--- le nom ce sera apres/Assets/Scripts/ScoreManager.cs
+++ le nom ce sera apres/Assets/Scripts/ScoreManager.cs
@@ -9,19 +9,8 @@ public class ScoreManager : MonoBehaviour
     public int collectibleScore = 200;
     private GameObject Player;
     public AnimationCurve animCurve;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        StartCoroutine(Movements());
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
+    private float graphValue;
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -30,27 +19,5 @@ public class ScoreManager : MonoBehaviour
             InfiniteMovement.instance.scorePlayer += collectibleScore;
             Destroy(gameObject);
         }
-    }
-
-    IEnumerator Movements()
-    {
-        for (int i = 0; i < 10; i++)
-        {
-            this.gameObject.transform.position += new Vector3(0f, 0.2f, 0f);
-            yield return new WaitForSeconds(0.01f);
-        }
-
-        yield return new WaitForSeconds(0.5f);
-        
-        for (int j = 0; j > 0; j--)
-        {
-            this.gameObject.transform.position -= new Vector3(0f, 0.2f, 0f);
-            yield return new WaitForSeconds(0.01f);
-        }
-        
-        yield return new WaitForSeconds(0.05f);
-
-        StartCoroutine(Movements());
-
     }
 }
