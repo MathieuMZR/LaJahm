b60d62ebd01d56caf17488853e79d7e5af5853a1
MathieuMZR <m.meziere@rubika-edu.com>
2022-03-06 04:42:06 +0100

    Score Changements

--- le nom ce sera apres/Assets/Scripts/ScoreManager.cs
+++ le nom ce sera apres/Assets/Scripts/ScoreManager.cs
@@ -6,28 +6,29 @@ using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
-    public ScoreHandler sH;
     public int collectibleScore = 200;
+    private GameObject Character;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    /*private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            sH.score += collectibleScore;
+            Character = other.gameObject;
+            Character.score += collectibleScore;
             Debug.Log(sH.score);
             Destroy(gameObject);
         }
-    }
+    }*/
 }
