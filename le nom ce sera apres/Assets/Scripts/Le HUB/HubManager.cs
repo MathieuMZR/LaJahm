c399e821dacc34e2f893779ea88047959a99a563
Aloiszz <a.tupinon@rubika-edu.com>
2022-03-06 13:33:26 +0100

    Le Hub fonctionnel

--- le nom ce sera apres/Assets/Scripts/Le HUB/HubManager.cs
+++ le nom ce sera apres/Assets/Scripts/Le HUB/HubManager.cs
@@ -20,6 +20,11 @@ public class HubManager : MonoBehaviour
     public GameObject Hub_2;
     public GameObject Hub_3;
 
+    private bool isHub1 = false;
+    private bool isHub2 = false;
+    private bool isHub3 = false;
+    
+
     public int index = 0;
     public bool canAcessToHub = false;
     private void OnTriggerEnter2D(Collider2D other)
@@ -37,43 +42,65 @@ public class HubManager : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (isHub1 == true)
+        {
+            Hub1();
+        }
+        else if (isHub2 == true)
+        {
+            Hub2();
+        }
+        else if (isHub3 == true)
+        {
+            Hub3();
+        }
+    }
+
     IEnumerator TimeWaitForUI()
     {
-        yield return new WaitForSeconds(3.5f);//Play Annnimation
-        Debug.Log("C'est l'heure des télétubbies");
-        
+        InfiniteMovement.instance.anim.SetBool("Plug", true);
+        yield return new WaitForSeconds(3.5f);//Play Annimation;
+
         if (index == 0) //Teleport player into hub 1
         {
             Player.transform.position = respawnPointHub1.transform.position;
             index += 1; 
-            StartCoroutine(ComeBackHub1());
+            //StartCoroutine(ComeBackHub1());
+            isHub1 = true;
+            InfiniteMovement.instance.anim.SetBool("Idle", true);
+            InfiniteMovement.instance.anim.SetFloat("Run", 0);
+
         }
         else if (index == 1)//Teleport player into hub 2
         {
             Player.transform.position = respawnPointHub2.transform.position;
             index += 1;
-            StartCoroutine(ComeBackHub2());
+            //StartCoroutine(ComeBackHub2());
+            isHub2 = true;
         }
         else if (index == 2) //Teleport player into hub 3
         {
             Player.transform.position = respawnPointHub3.transform.position;
-            StartCoroutine(ComeBackHub3());
+            //StartCoroutine(ComeBackHub3());
+            isHub3 = true;
         }
+        
     }
-
-
+    
     private void Awake()
     {
         Instantiate(Hub_1, new Vector3(-62.68461f, -15.13549f, 0f), Quaternion.identity); //Hub 1
         Instantiate(Hub_2, new Vector3(-73f, -21f, 0f), Quaternion.identity); //Hub 2
         Instantiate(Hub_3, new Vector3(-48f, -21f, 0f), Quaternion.identity); //Hub 3
     }
+    
 
     IEnumerator ComeBackHub1()
     {
         yield return new WaitForSeconds(1f);
         Player.transform.position = respawnPointModule1.transform.position;
-        Debug.Log("Hello");
         newMoveSpeed = 20f;
         yield return new WaitForSeconds(0.1f);
         RespawnPointModule1.SetActive(false);
@@ -102,5 +129,28 @@ public class HubManager : MonoBehaviour
         RespawnPointModule1.SetActive(true);
         newMoveSpeed = 0f;
     }
+
+    void Hub1()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(ComeBackHub1());
+        }
+    }
     
+    void Hub2()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(ComeBackHub2());
+        }
+    }
+    
+    void Hub3()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(ComeBackHub3());
+        }
+    }
 }
